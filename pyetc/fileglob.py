44088ed3ef9eb57e637083eb090d0c7b82256253
Yokota Yoshinori <yokota@yynet.org>
2026-01-12 10:07:12 +0900

    Update.

--- pyetc/fileglob.py
+++ pyetc/fileglob.py
@@ -1,17 +1,30 @@
 import glob
+import logging
 import os
-from get_gitignore import get_gitignore
 from fnmatch import fnmatch
+
+from get_gitignore import get_gitignore
+
+logger = logging.getLogger(__name__)
+
 
 def file_glob(pattern: str, root_path: str = "."):
     ignore_pattern = get_gitignore(root_path)
-    print("ignore_pattern is", sorted(list(ignore_pattern)))
+    logger.debug(f"ignore_pattern is {sorted(ignore_pattern)}")
+
+    files = []
     for file in glob.glob(os.path.join(root_path, pattern), recursive=True):
-        file = os.path.relpath(file, root_path)
+        file = os.path.relpath(file, root_path)  # 相対パス
         if not file or os.path.isdir(file):
             continue
-        a = any(fnmatch(file, p) for p in ignore_pattern)
-        print(file, a)
+        if any(fnmatch(file, p) for p in ignore_pattern):
+            logger.debug(f"ignore {file}")
+            continue
+        files.append(file)
+    return files
+
 
 if __name__ == "__main__":
-    file_glob("**/*.py", ".")
+    logging.basicConfig(level=logging.DEBUG)
+    logging.getLogger("get_gitignore").setLevel(logging.INFO)
+    print(file_glob("**/*.py"))
--- pyetc/get_gitignore.py
+++ pyetc/get_gitignore.py
@@ -1,5 +1,8 @@
 import glob
+import logging
 import os
+
+logger = logging.getLogger(__name__)
 
 
 def get_gitignore(root_path: str = "."):
@@ -7,8 +10,9 @@ def get_gitignore(root_path: str = "."):
     for file in glob.glob(
         os.path.join(root_path, "**/.gitignore"), recursive=True
     ):
+        logger.debug(f"'{file}' found.")
         d = os.path.dirname(file)
-        with open(file, "r", encoding="utf-8") as fd:
+        with open(file, "r", encoding="utf-8", errors="ignore") as fd:
             for line in fd:
                 line = line.strip()
                 if not line or line.startswith("#"):
@@ -20,12 +24,16 @@ def get_gitignore(root_path: str = "."):
 
                 # パスを結合してから正規化
                 norm_path = os.path.normpath(os.path.join(d, line))
+                logger.debug(f"norm; {norm_path}")
                 ignore_glob.add(norm_path)
+
                 norm_path = os.path.normpath(os.path.join(d, "*", line))
+                logger.debug(f"norm; {norm_path}")
                 ignore_glob.add(norm_path)
 
     return list(ignore_glob)
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
     print(get_gitignore())
